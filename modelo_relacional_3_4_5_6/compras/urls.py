1d33505a534e8a86d68f8dc01ac5aaa4f688e51c
JATeR912 <jater.code@gmail.com>
2025-11-05 00:31:40 -0300

    Ajustes message y urls vacia

--- modelo_relacional_3_4_5_6/compras/urls.py
+++ modelo_relacional_3_4_5_6/compras/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productos/', include('producto_app.urls')),
+    path('', include('producto_app.urls')),
 ]
