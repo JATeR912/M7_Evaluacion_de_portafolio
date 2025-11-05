94114278610f7b7331a1d7db365c18a636ecbbf0
JATeR912 <jater.code@gmail.com>
2025-11-04 22:37:50 -0300

    migraciones y modificacion modelos

--- modelo_relacional_3_4_5_6/compras/urls.py
+++ modelo_relacional_3_4_5_6/compras/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productos/', include('compras_app.urls')),
+    path('productos/', include('producto_app.urls')),
 ]
--- modelo_relacional_3_4_5_6/producto_app/models.py
+++ modelo_relacional_3_4_5_6/producto_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.PositiveIntegerField(default=0)
+    descripcion = models.TextField(default='Sin descripción')
 
     def __str__(self):
         return self.nombre
